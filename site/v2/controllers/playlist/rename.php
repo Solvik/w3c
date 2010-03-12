6d1c9bacc4a8faa72752a34e12e8083e2e593516
Solvik Blum <solvik.blum@gmail.com>
2010-03-12 15:48:45 +0000

    Ca doit être bon

--- site/v2/controllers/playlist/rename.php
+++ site/v2/controllers/playlist/rename.php
@@ -5,7 +5,7 @@ if(!is_online()) { include NON_CONNECTE; exit(); }
 $compte = new Member(Member::EXISTANT, $_SESSION['login']);
 $stream = $compte->getStream();
 
-if (!empty($_GET['playlist'])) // Si le formulaire a été envoyé
+if (!empty($_POST['nom'])) // Si le formulaire a été envoyé
   {
     $playlistId = intval($_GET['playlist']);
     $playlist = new PlayList($playlistId, $compte, $stream);
