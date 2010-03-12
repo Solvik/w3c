0b2e508697a90be2f54c1226822379556190f1f4
Solvik Blum <solvik.blum@gmail.com>
2010-03-12 15:26:32 +0000

    rename de playlist

---
+++ site/v2/controllers/playlist/rename.php
@@ -0,0 +1,20 @@
+<?php
+include MODEL.'Musiques.php';
+if(!is_online()) { include NON_CONNECTE; exit(); }
+
+$compte = new Member(Member::EXISTANT, $_SESSION['login']);
+$stream = $compte->getStream();
+
+if (!empty($_GET['playlist'])) // Si le formulaire a été envoyé
+  {
+    $playlistId = intval($_GET['playlist']);
+    $playlist = new PlayList($playlistId, $compte, $stream);
+	
+    if(PlayList::belongs($playlist, $compte))
+      {
+	$playlist->nom = $_POST['name'];
+	$playlist->save();
+	include VIEW.'rename-playlist-ok.html';
+      }
+	
+  }
