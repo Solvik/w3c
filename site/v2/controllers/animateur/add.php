81277600d775c1c21de76f2200cae49cf8ec9a18
Solvik Blum <solvik.blum@gmail.com>
2010-07-01 21:41:28 +0000

    Une fonction avait disparue ... (un voleur de fonction ? :P)

--- site/v2/controllers/animateur/add.php
+++ site/v2/controllers/animateur/add.php
@@ -19,8 +19,8 @@ elseif (isset($_POST['add_creneaux']))
 			$heure_debut = '00:00:00';
 			$heure_fin = '23:59:59';
 		} else {
-			$heure_debut = nb_with_zero(htmlspecialchars($_POST['hdebut'])).":".nb_with_zero(htmlspecialchars($_POST['mdebut'])).":00";
-			$heure_fin = nb_with_zero(htmlspecialchars($_POST['hfin'])).":".nb_with_zero(htmlspecialchars($_POST['mfin'])).":00";
+			$heure_debut = time_format(intval($_POST['hdebut']), intval($_POST['mdebut'])).":00";
+			$heure_fin = time_format(intval($_POST['hfin']), intval($_POST['mfin'])).":00";
 		}
 		
 		Creneaux::addCreneau($compte,							
