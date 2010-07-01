a571c07e8a8ab42d7e0d5de00eaaf050e5c8f974
Solvik Blum <solvik.blum@gmail.com>
2010-07-01 21:37:23 +0000

    Correction style + erreur

--- site/v2/controllers/animateur/add.php
+++ site/v2/controllers/animateur/add.php
@@ -14,13 +14,13 @@ elseif (isset($_POST['add_creneaux']))
 {
 	if($_POST['jour'] >= 1 AND $_POST['jour'] <= 8)
 	{
-		$heure_debut = nb_with_zero(htmlspecialchars($_POST['hdebut'])).":".nb_with_zero(htmlspecialchars($_POST['mdebut'])).":00";
-		$heure_fin = nb_with_zero(htmlspecialchars($_POST['hfin'])).":".nb_with_zero(htmlspecialchars($_POST['mfin'])).":00";
-
 		if($_POST['jour'] == 8)
 		{
 			$heure_debut = '00:00:00';
 			$heure_fin = '23:59:59';
+		} else {
+			$heure_debut = nb_with_zero(htmlspecialchars($_POST['hdebut'])).":".nb_with_zero(htmlspecialchars($_POST['mdebut'])).":00";
+			$heure_fin = nb_with_zero(htmlspecialchars($_POST['hfin'])).":".nb_with_zero(htmlspecialchars($_POST['mfin'])).":00";
 		}
 		
 		Creneaux::addCreneau($compte,							
